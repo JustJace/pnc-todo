b8b560345ed1a9dedfb0bb8d34cce0e1ba43eb61
Jace Brewster <jacebrewster@gmail.com>
2021-04-04 18:39:04 -0500

    simplify signin component

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { delay, filter, finalize, map, tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-sign-in',
@@ -9,9 +12,10 @@ import { AuthService } from '../services/auth.service';
 })
 export class SignInComponent implements OnInit {
 
-  constructor(public readonly auth: AuthService, private readonly _loading: LoadingController) { }
+  constructor(private readonly auth: AuthService, private readonly _loading: LoadingController) { }
 
   ngOnInit(): void {
+  
   }
 
   public async signInWithGoogle(): Promise<void> {
